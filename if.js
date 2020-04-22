4d965f022000915ed19e32256e847f806280ee16
OYEBAMIJI <HARYOURBHAMIE@AOL.COM>
2020-04-22 12:37:18 +0100

    if and while loop was added

---
+++ if.js
@@ -0,0 +1,26 @@
+let numbers=[];
+numbers[1,3,1,3,5,6,7,5,4,3,5,6,4];
+//if (numbers[0]==numbers[2]) {
+ //   console.log('correct')
+//}
+//IF ELSE EXAMPLE
+if(numbers[4]==numbers[5]){
+    console.log('correct');
+}
+else{
+    console.log('wrong, try again')
+}
+if(numbers[0]==numbers[2] && numbers[2]==numbers[6]){
+    console.log('correct')
+}
+else{
+    console.log('wrong, try again')
+}
+
+if(numbers[0]==numbers[2] || numbers[2]==numbers[6]){
+    console.log('correct')
+}
+else{
+    console.log('wrong, try again')
+}
+
